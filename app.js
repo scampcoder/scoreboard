194e041ce5e602b91e4cd733afb6a510c902f3d8
Victoria Kee <victoriakee95@gmail.com>
2021-11-10 10:55:59 -0600

    Experiment with JSX

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
-const title = React.createElement(
-  'h1', //type
-  { id: 'main-title', title: 'This is a title.' }, //props
-  'My first React element!'//children
+const desc = 'I just learned how to create a React node and render it into the DOM.'
+const myTitleID = 'main-title';
+const name = 'Victoria';
+
+const header = (
+    <header>
+    {/* this is a comment in JSX */}
+        <h1 id={ myTitleID }>{ name }'s First React Element</h1>
+        <p>{ desc }</p>
+    </header>
 );
-
-// when page is rendered we should be shown:
-// <h1 id="main-title" title="This is a title">My first React element!</h1>
-
-//console.log(title); //this will show up as an object
-
-const desc = React.createElement(
-    'p',
-    null,
-    'I just learned how to create a React node and render it into the DOM.'
-);
-
-const header = React.createElement(
-    'header',
-    null,
-    title, desc
-)
 
 ReactDOM.render(
     header, //element to render
     document.getElementById('root') //container it will render in
 );
+
+//when we write curly braces it lets JSX know that we are using JS.
+//Inside the curly braces is what's called a JS espression.
