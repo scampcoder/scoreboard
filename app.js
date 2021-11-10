eb8827e6864484c6709a729e3c1f6b97d5d0f864
Victoria Kee <victoriakee95@gmail.com>
2021-11-10 17:12:57 -0600

    lift score state

--- app.js
+++ app.js
@@ -36,17 +36,24 @@ const Player = (props) => {
             <span className="player-name">
                 { props.name }
             </span>
-            <Counter score={ props.score }/>
+            <Counter />
         </div>
     )
 }
 
 class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            score: 0,
+        };
+    }
+
     render() {
         return (
             <div className="counter">
                 <button className="counter-action decrement"> - </button>
-                <span className="counter-score">{ this.props.score }</span>
+                <span className="counter-score">{ this.state.score }</span>
                 <button className="counter-action increment"> + </button>
             </div>
         );
@@ -63,7 +70,6 @@ const App = (props) => {
             {props.initialPlayers.map( player => 
                 <Player
                     name={player.name}
-                    score={player.score}
                     key={player.id.toString()}
                 />
             )}
