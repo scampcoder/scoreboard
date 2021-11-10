d7d1a7e87a2f185ec6b46d7b35d0c6b96f30f4e0
Victoria Kee <victoriakee95@gmail.com>
2021-11-10 14:35:57 -0600

    finish passing props/setup for player

--- app.js
+++ app.js
@@ -7,22 +7,22 @@ const Header = (props) => {
     );
 }
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                Victoria
+                { props.name }
             </span>
-            <Counter />
+            <Counter score={ props.score }/>
         </div>
     )
 }
 
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-            <span className="counter-score">35</span>
+            <span className="counter-score">{ props.score }</span>
             <button className="counter-action increment"> + </button>
         </div>
     );
@@ -33,7 +33,7 @@ const App = () => {
         <div className="scoreboard">
             <Header title="Scoreboard" totalPlayers={1} />
             {/* Player's List */}
-            <Player />
+            <Player name="Victoria" score={50}/>
         </div>
     );
 }
