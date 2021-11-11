5936abd59da76c68579f72dd1bfa1ecd10072d67
Victoria Kee <victoriakee95@gmail.com>
2021-11-11 11:23:17 -0600

    rewrite incScore as arrow function to bind

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ class Counter extends React.Component {
             score: 0,
         };
 
-    incrementScore() {
+    incrementScore = () => {
         this.setState({
             score: this.state.score + 1
         });
@@ -57,7 +57,7 @@ class Counter extends React.Component {
             <div className="counter">
                 <button className="counter-action decrement"> - </button>
                 <span className="counter-score">{ this.state.score }</span>
-                <button className="counter-action increment" onClick={this.incrementScore.bind(this)}> + </button>
+                <button className="counter-action increment" onClick={this.incrementScore}> + </button>
             </div>
         );
     }
